18182fd1cffb1baabaab7472da61721c3c79851e
Tom Batchelor <tom.batchelor@me.com>
2017-10-24 17:52:15 -0700

    Fixed comments

--- getMetrics.py
+++ getMetrics.py
@@ -8,14 +8,16 @@ password = ''
 customerName = ''
 controller = ''
 
-# Mobile app details, can be found in the URL when looking at mobile data
+# App details, can be found in the URL when looking at mobile data
 appName = ''
-g
 
-# Login to controller and setup session
+
+# Login to controller and get session and CSRF tokens
 response = requests.get(controller + 'controller/auth?action=login', auth=(username + '@' + customerName, password))
 jsessionID = response.cookies.get('JSESSIONID')
 csrf = response.cookies.get('X-CSRF-TOKEN')
+
+# Add JSESSION ID to cookies and CSRF token to headers
 cookies = {'JSESSIONID' : jsessionID}
 headers = {'X-CSRF-TOKEN' : csrf}
 
